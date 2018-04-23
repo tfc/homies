cc3b029d48f6f23c9395aae7a40a764dc1d8a7bc
Jacek Galowicz <jacek.galowicz@cyberus-technology.de>
2018-04-23 16:32:35 +0200

    try to make it hydra compatible

--- default.nix
+++ default.nix
@@ -2,60 +2,21 @@
 # instructions.
 { pkgs ? import <nixpkgs> {} }:
 let
+  customPkgs = import ./release.nix { pkgs = pkgs; };
+
   # The list of packages to be installed
-  homies = [
+  homies = (with customPkgs; [
       bashrc
       git
       tmux
       vim
-    ] ++ (with pkgs; [
+    ]) ++ (with pkgs; [
       curl
       htop
       mosh
       nix
       sloccount
     ]);
-
-  ## Some cunstomizations
-
-  # A custom '.bashrc' (see bashrc/default.nix for details)
-  bashrc = import ./bashrc (with pkgs;
-    { inherit
-        writeScriptBin
-        ;
-    });
-
-  # Git with config baked in
-  git = import ./git (with pkgs;
-    { inherit
-        makeWrapper
-        symlinkJoin
-        ;
-      git = pkgs.git;
-    });
-
-  # Tmux with a custom tmux.conf baked in
-  tmux = import ./tmux (with pkgs;
-    { inherit
-        makeWrapper
-        symlinkJoin
-        writeText
-        ;
-      tmux = pkgs.tmux;
-    });
-
-
-  # Vim with a custom vimrc and set of packages
-  vim = import ./vim (with pkgs;
-    {inherit
-        symlinkJoin
-        makeWrapper
-        vim_configurable
-        vimUtils
-        vimPlugins
-        haskellPackages;
-    });
-
 in
   if pkgs.lib.inNixShell
   then pkgs.mkShell
--- release.nix
+++ release.nix
@@ -1,7 +1,41 @@
-{ p }:
-let
-  homies = (import ./default.nix { pkgs = p; });
-in {
-  vim = homies.vim;
-  tmux = homies.tmux;
-}
+{ pkgs ? import <nixpkgs> {} }:
+
+{
+  # A custom '.bashrc' (see bashrc/default.nix for details)
+  bashrc = import ./bashrc (with pkgs;
+    { inherit
+        writeScriptBin
+        ;
+    });
+
+  # Git with config baked in
+  git = import ./git (with pkgs;
+    { inherit
+        makeWrapper
+        symlinkJoin
+        ;
+      git = pkgs.git;
+    });
+
+  # Tmux with a custom tmux.conf baked in
+  tmux = import ./tmux (with pkgs;
+    { inherit
+        makeWrapper
+        symlinkJoin
+        writeText
+        ;
+      tmux = pkgs.tmux;
+    });
+
+
+  # Vim with a custom vimrc and set of packages
+  vim = import ./vim (with pkgs;
+    {inherit
+        symlinkJoin
+        makeWrapper
+        vim_configurable
+        vimUtils
+        vimPlugins
+        haskellPackages;
+    });
+  }
